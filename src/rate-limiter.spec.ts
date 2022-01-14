ce97119cc9d9c5c4b60797b1fcc7f79012445065
Ricky Clegg <rickyclegg@CTM-MAC-040031.bgl.financial.local>
2022-01-14 15:38:09 +0000

    refactor: allowed easy multiple calls in tests

--- src/rate-limiter.spec.ts
+++ src/rate-limiter.spec.ts
@@ -2,40 +2,55 @@
 // A rate limiter can limit by requests x number per second
 // Given {id: "xxxxxxx"} it will return a boolean to represent if the action is allowed
 import RateLimiter from './rate-limiter'
-import {Limitless} from './types'
+import {Limitless, AllowParams} from './types'
 
 describe("Rate limiter", () => {
   let rl: Limitless
 
-  const createRandomParams = () => ({
-    id: Math.floor(Math.random() * 10000).toString()
+  const getRandomInt = (): number => Math.floor(Math.random() * 1000)
+
+  const createRandomParams = (): AllowParams => ({
+    id: getRandomInt().toString()
   })
+
+  const assertAllowedCalls = (params: AllowParams, calls: number): void => {
+    for(let i = 0; i < calls; i++) {
+      expect(rl.isAllowed(params)).toBe(true)
+    }
+  }
+
+  const assertBlockedCalls = (params: AllowParams, calls: number) => {
+    for(let i = 0; i < calls; i++) {
+      expect(rl.isAllowed(params)).toBe(false)
+    }
+  }
 
   it('should NOT allow calling anything beyond rate', () => {
     const params1 = createRandomParams()
     const params2 = createRandomParams()
+    const allowedCalls = 1
 
-    rl = new RateLimiter({allowedCalls: 1})
+    rl = new RateLimiter({allowedCalls})
 
-    expect(rl.isAllowed(params1)).toBe(true)
-    expect(rl.isAllowed(params1)).toBe(false)
-    expect(rl.isAllowed(params2)).toBe(true)
+    assertAllowedCalls(params1, allowedCalls)
+    assertBlockedCalls(params1, allowedCalls)
+    assertAllowedCalls(params2, allowedCalls)
   })
 
   it('should allow calling rate after timeout', () => {
     const params1 = createRandomParams()
+    const allowedCalls = getRandomInt()
 
     jest.useFakeTimers();
 
-    rl = new RateLimiter({allowedCalls: 2, timeperiod: 1000})
+    rl = new RateLimiter({allowedCalls, timeperiod: 1000})
     
-    expect(rl.isAllowed(params1)).toBe(true)
-    expect(rl.isAllowed(params1)).toBe(true)
-    expect(rl.isAllowed(params1)).toBe(false)
+    assertAllowedCalls(params1, allowedCalls)
+    assertBlockedCalls(params1, getRandomInt())
 
     jest.advanceTimersByTime(1001)
 
-    expect(rl.isAllowed(params1)).toBe(true)
+    assertAllowedCalls(params1, allowedCalls)
 
     jest.useRealTimers();
   })
