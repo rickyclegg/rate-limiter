f89b2a7380829971fb7584e3bf9a89569166476f
Ricky Clegg <rickyclegg@CTM-MAC-040031.bgl.financial.local>
2022-01-14 15:19:40 +0000

    refactor: magic objects into createRandomParams

--- src/rate-limiter.spec.ts
+++ src/rate-limiter.spec.ts
@@ -7,26 +7,35 @@ import {Limitless} from './types'
 describe("Rate limiter", () => {
   let rl: Limitless
 
+  const createRandomParams = () => ({
+    id: Math.floor(Math.random() * 10000).toString()
+  })
+
   it('should NOT allow calling anything beyond rate', () => {
+    const params1 = createRandomParams()
+    const params2 = createRandomParams()
+
     rl = new RateLimiter({allowedCalls: 1})
 
-    expect(rl.isAllowed({id: 'A'})).toBe(true)
-    expect(rl.isAllowed({id: 'A'})).toBe(false)
-    expect(rl.isAllowed({id: 'B'})).toBe(true)
+    expect(rl.isAllowed(params1)).toBe(true)
+    expect(rl.isAllowed(params1)).toBe(false)
+    expect(rl.isAllowed(params2)).toBe(true)
   })
 
   it('should allow calling rate after timeout', () => {
+    const params1 = createRandomParams()
+
     jest.useFakeTimers();
 
     rl = new RateLimiter({allowedCalls: 2, timeperiod: 1000})
     
-    expect(rl.isAllowed({id: 'A'})).toBe(true)
-    expect(rl.isAllowed({id: 'A'})).toBe(true)
-    expect(rl.isAllowed({id: 'A'})).toBe(false)
+    expect(rl.isAllowed(params1)).toBe(true)
+    expect(rl.isAllowed(params1)).toBe(true)
+    expect(rl.isAllowed(params1)).toBe(false)
 
     jest.advanceTimersByTime(1001)
 
-    expect(rl.isAllowed({id: 'A'})).toBe(true)
+    expect(rl.isAllowed(params1)).toBe(true)
 
     jest.useRealTimers();
   })
