de16b55783cb21e9f95aef4665f1cca954ea6749
Ricky Clegg <rickyclegg@CTM-MAC-040031.bgl.financial.local>
2022-01-17 13:30:55 +0000

    refactor: rename calls to memory

--- src/buckets/mem-bucket.ts
+++ src/buckets/mem-bucket.ts
@@ -2,19 +2,19 @@ import {AllowParams} from '../types'
 import { Bucket } from './types';
 
 export class MemBucket implements Bucket {
-    private calls: Record<string, number> = {}
+    private memory: Record<string, number> = {}
 
     public async set(id: AllowParams['id'], increment: number) : Promise<void> {
         let numberOfCalls = await this.get(id)
         numberOfCalls += increment;
-        this.calls[id] = numberOfCalls;
+        this.memory[id] = numberOfCalls;
     }
 
     public async get(id: AllowParams['id']): Promise<number> {
-        return this.calls[id] ?? 0
+        return this.memory[id] ?? 0
     }
 
     public async delete(id: AllowParams['id']): Promise<void> {
-        delete this.calls[id]
+        delete this.memory[id]
     }
 }
