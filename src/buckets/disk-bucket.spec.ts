38cccb3569af8ae4f478f82a135479b9ac241cbf
Ricky Clegg <rickyclegg@CTM-MAC-040031.bgl.financial.local>
2022-01-18 16:26:46 +0000

    feat: writes one property to disk

--- src/buckets/disk-bucket.spec.ts
+++ src/buckets/disk-bucket.spec.ts
@@ -7,9 +7,11 @@ describe("Disk Bucket", () => {
     const stubDiskReader = async () => {
       return JSON.stringify({ [id]: numberOfCalls });
     };
+    const dummyDiskWriter = jest.fn().mockResolvedValue(undefined);
 
     const db = new DiskBucket({
       reader: stubDiskReader,
+      writer: dummyDiskWriter,
       filename: "dummyFilename",
     });
 
@@ -22,12 +24,36 @@ describe("Disk Bucket", () => {
     const stubDiskReader = async () => {
       return JSON.stringify({});
     };
+    const dummyDiskWriter = jest.fn().mockResolvedValue(undefined);
 
     const db = new DiskBucket({
       reader: stubDiskReader,
+      writer: dummyDiskWriter,
       filename: "dummyFilename",
     });
 
     expect(await db.get(id)).toEqual(numberOfCalls);
+  });
+
+  it("should call the writer to store the bucket in memory", async () => {
+    const id = "myTestKey";
+    const numberOfCalls = 0;
+    const stubDiskReader = async () => {
+      return JSON.stringify({});
+    };
+    const stubDiskWriter = jest.fn().mockResolvedValue(undefined);
+
+    const db = new DiskBucket({
+      reader: stubDiskReader,
+      writer: stubDiskWriter,
+      filename: "dummyFilename",
+    });
+
+    await db.set(id, 1);
+
+    expect(stubDiskWriter).toHaveBeenCalledWith(
+      "dummyFilename",
+      JSON.stringify({ [id]: numberOfCalls + 1 })
+    );
   });
 });
--- src/buckets/disk-bucket.ts
+++ src/buckets/disk-bucket.ts
@@ -4,6 +4,7 @@ import { AllowParams } from "../types";
 interface Options {
   filename: string;
   reader(filename: string, encoding: "utf8"): Promise<string>;
+  writer(filename: string, data: string): Promise<void>;
 }
 
 export class DiskBucket implements Bucket {
@@ -15,13 +16,19 @@ export class DiskBucket implements Bucket {
 
   public async get(id: AllowParams["id"]): Promise<number> {
     const rawFile = await this.options.reader(this.options.filename, "utf8");
-    const memory: Record<string, number> = JSON.parse(rawFile)
+    const memory: Record<string, number> = JSON.parse(rawFile);
 
-    return memory[id] ?? 0
+    return memory[id] ?? 0;
   }
 
-  public set(id: AllowParams["id"], increment: number): Promise<void> {
-    return Promise.resolve(undefined);
+  public async set(id: AllowParams["id"], increment: number): Promise<void> {
+    let numberOfCalls = await this.get(id);
+    numberOfCalls += increment;
+
+    await this.options.writer(
+      this.options.filename,
+      JSON.stringify({ [id]: numberOfCalls })
+    );
   }
 
   public delete(id: AllowParams["id"]): Promise<void> {
