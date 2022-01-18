4b3007dc8dc81bc8b8d2aed5770d7815b380e4be
Ricky Clegg <rickyclegg@CTM-MAC-040031.bgl.financial.local>
2022-01-18 16:37:57 +0000

    refactor: magic strings in tests

--- src/buckets/disk-bucket.spec.ts
+++ src/buckets/disk-bucket.spec.ts
@@ -4,53 +4,40 @@ describe("Disk Bucket", () => {
   const createTestDeps = () => ({
     reader: jest.fn().mockResolvedValue("{}"),
     writer: jest.fn().mockResolvedValue(undefined),
+    id: `key-${Math.floor(Math.random() * 100000)}`,
+    filename: `filename-${Math.floor(Math.random() * 100000)}`,
   });
 
   it("should get me the call count from disk", async () => {
-    const id = "myTestKey";
     const numberOfCalls = 3;
-    const { reader, writer } = createTestDeps();
+    const { id, filename, reader, writer } = createTestDeps();
 
     reader.mockResolvedValue(JSON.stringify({ [id]: numberOfCalls }));
 
-    const db = new DiskBucket({
-      reader,
-      writer,
-      filename: "dummyFilename",
-    });
+    const db = new DiskBucket({ reader, writer, filename });
 
     expect(await db.get(id)).toEqual(numberOfCalls);
   });
 
   it("should return 0 for call counts that do not exist", async () => {
-    const id = "myTestKey";
     const numberOfCalls = 0;
-    const { reader, writer } = createTestDeps();
+    const { id, filename, reader, writer } = createTestDeps();
 
-    const db = new DiskBucket({
-      reader,
-      writer,
-      filename: "dummyFilename",
-    });
+    const db = new DiskBucket({ reader, writer, filename });
 
     expect(await db.get(id)).toEqual(numberOfCalls);
   });
 
   it("should call the writer to store the bucket in memory", async () => {
-    const id = "myTestKey";
     const numberOfCalls = 0;
-    const { reader, writer } = createTestDeps();
+    const { id, filename, reader, writer } = createTestDeps();
 
-    const db = new DiskBucket({
-      reader,
-      writer,
-      filename: "dummyFilename",
-    });
+    const db = new DiskBucket({ reader, writer, filename });
 
     await db.set(id, 1);
 
     expect(writer).toHaveBeenCalledWith(
-      "dummyFilename",
+      filename,
       JSON.stringify({ [id]: numberOfCalls + 1 })
     );
   });
