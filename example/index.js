f4fddf04133902ffcab990a6f233965a5477dd93
Ricky Clegg <rickyclegg@CTM-MAC-040031.bgl.financial.local>
2022-01-19 10:08:36 +0000

    feat: test DiskBucket in example

--- example/index.js
+++ example/index.js
@@ -1,33 +1,42 @@
 const RateLimiter = require("../dist/rate-limiter").default;
 const { MemBucket } = require("../dist/buckets/mem-bucket");
+const { DiskBucket } = require("../dist/buckets/disk-bucket");
+const fs = require('fs')
+const util = require('util')
 
 const BUCKET_CLEAR_TIME = 10000;
 const ATTEMPTS = 3;
 
-(async function main () {
+(async function main() {
   const options = {
     allowedCalls: ATTEMPTS,
     timeperiod: BUCKET_CLEAR_TIME,
-    bucket: new MemBucket()
-  }
-  const rl = new RateLimiter(options)
+    bucket: new DiskBucket({
+      filename: './memory.json',
+      reader: util.promisify(fs.readFile),
+      writer: util.promisify(fs.writeFile),
+    }),
+  };
+  const rl = new RateLimiter(options);
 
   function colorize(color, output) {
-    return ['\033[', color, 'm', output, '\033[0m'].join('');
+    return ["\033[", color, "m", output, "\033[0m"].join("");
   }
 
-  console.log(`Rate Limiter - ${ATTEMPTS} allowed every ${BUCKET_CLEAR_TIME / 1000}s`);
+  console.log(
+    `Rate Limiter - ${ATTEMPTS} allowed every ${BUCKET_CLEAR_TIME / 1000}s`
+  );
 
   let requestsMade = 1;
 
   setInterval(() => {
-    requestsMade = 1
+    requestsMade = 1;
 
     console.log(`\nReset limit!`);
-  }, BUCKET_CLEAR_TIME)
+  }, BUCKET_CLEAR_TIME);
 
   setInterval(async () => {
-    const isAllowed = await rl.isAllowed({id: 'example'})
+    const isAllowed = await rl.isAllowed({ id: "example" });
 
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
@@ -35,17 +44,18 @@ const ATTEMPTS = 3;
 
     setTimeout(() => {
       process.stdout.write(`.`);
-    }, 500)
+    }, 500);
 
     setTimeout(() => {
       process.stdout.write(`.`);
-    }, 800)
+    }, 800);
 
     setTimeout(() => {
-      process.stdout.write(isAllowed ? colorize(92, ' Allowed') : colorize(91, ' Blocked'));
+      process.stdout.write(
+        isAllowed ? colorize(92, " Allowed") : colorize(91, " Blocked")
+      );
 
-      requestsMade += 1
-    }, 1000)
-  }, Math.floor(BUCKET_CLEAR_TIME / 5))
-}())
-
+      requestsMade += 1;
+    }, 1000);
+  }, Math.floor(BUCKET_CLEAR_TIME / 5));
+})();
